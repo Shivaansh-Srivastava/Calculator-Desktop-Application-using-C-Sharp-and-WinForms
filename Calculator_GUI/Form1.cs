fc5fbdd0cec0f398349e4bfa1fb07972487aa4c4
Shivaansh-Srivastava <72780341+Shivaansh-Srivastava@users.noreply.github.com>
2021-10-05 16:24:49 +0530

    first commit

---
+++ Calculator_GUI/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator_GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n1 = Convert.ToInt32(textBox1.Text);
+            int n2 = Convert.ToInt32(textBox2.Text);
+            int sum = n1 + n2;
+            textBox3.Text = sum.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int n1 = Convert.ToInt32(textBox1.Text);
+            int n2 = Convert.ToInt32(textBox2.Text);
+            int diff = (n1 > n2) ? n1 - n2 : n2 - n1;
+            textBox3.Text = diff.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int n1 = Convert.ToInt32(textBox1.Text);
+            int n2 = Convert.ToInt32(textBox2.Text);
+            int product = n1 * n2;
+            textBox3.Text = product.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int n1 = Convert.ToInt32(textBox1.Text);
+            int n2 = Convert.ToInt32(textBox2.Text);
+            int divide = (n1 > n2) ? n1 / n2 : n2 / n1;
+            textBox3.Text = divide.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+    }
+}
